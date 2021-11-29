428272bb432aeef56cf079bd0726c87ef2d9fa12
rehoosa <iuhoosa@gmail.com>
2021-11-29 23:25:06 +0900

    Modify crawller.py
    Add Selection

--- slave/crawller.py
+++ slave/crawller.py
@@ -2,19 +2,6 @@ from urllib.request import urlopen
 from urllib.parse import quote_plus
 from bs4 import BeautifulSoup
 import os
-#현재는 네이버만 가능
-baseUrl = 'https://search.naver.com/search.naver?where=image&sm=tab_jum&query=' # 네이버 검색url
-plusUrl = input('검색어를 입력하세요 : ') # 검색어 질문
-url = baseUrl + quote_plus(plusUrl) # url로 이동하기위한 쿼리문자열 만들기
-
-print(url)
-html  = urlopen(url) # url 열기
-bsObject = BeautifulSoup(html, 'html.parser')
-
-# for img in bsObject.find_all(name="img",limit=10): 
-#     print(img['src']) # 이미지의 src를 출력하는 부분
-
-# print(os.getcwd()) # os에서 현재 주소를 가져
 
 def createFolder(directory):    # 이미지 저장할 폴더를 생성하는 함수
     try:
@@ -36,5 +23,35 @@ def downloadPic(): # 이미지를 다운받는 함수
         n += 1
     print('다운로드 완료')
     
-createFolder('img')
-downloadPic()
+def showListImageSrc():
+    for img in bsObject.find_all(name="img",limit=10): 
+            print(img['src']) # 이미지의 src를 출력하는 부분
+
+
+
+baseUrl = 'https://search.naver.com/search.naver?where=image&sm=tab_jum&query=' # 네이버 검색url
+
+while(1):
+    #현재는 네이버만 가능
+    plusUrl = input('검색어를 입력하세요 : ') # 검색어 질문
+    url = baseUrl + quote_plus(plusUrl) # url로 이동하기위한 쿼리문자열 만들기
+
+    print(url)
+    html  = urlopen(url) # url 열기
+    bsObject = BeautifulSoup(html, 'html.parser')
+
+    # print(os.getcwd()) # os에서 현재 주소를 가져옴
+    
+    ifDownload = input('다운로드 하시겠습니까? 1 입력시 다운O, 아닐시 다운X')
+    if ifDownload == '1' :
+        createFolder('img')
+        downloadPic()
+    
+    ifShowList = input('주소를 보시겠습니까? 1 입력시 출력')
+    if ifShowList == '1' :
+        showListImageSrc()
+    
+    ifExit = input('종료하시겠습니까? 1 입력시 종료')
+    if ifExit == '1' : 
+        break
+    
